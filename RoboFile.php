52f4a2fc5bf4589a232bb075409bae51f20adb94
Will Long <will@kerasai.com>
2019-02-14 17:40:46 -0600

    added build tooling

---
+++ RoboFile.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks {
+
+  /**
+   * Run tests.
+   */
+  public function test() {
+    $this->testPhpcs();
+    $this->testPhpunit();
+  }
+
+  /**
+   * PHPCS code style checks.
+   */
+  public function testPhpcs() {
+    $this->taskExecStack()
+      ->exec('vendor/bin/phpcs --"standard=vendor/drupal/coder/coder_sniffer/Drupal" src')
+      ->run();
+  }
+
+  /**
+   * PHPUnit tests.
+   */
+  public function testPhpunit() {
+    $this->taskPhpUnit()->run();
+  }
+
+}
---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kerasai\Robo\Config\Tests\Unit;
+
+/**
+ * Class UnitTest.
+ */
+class UnitTest extends \PHPUnit\Framework\TestCase {
+
+  /**
+   * Unit test to ensure PHPUnit is running.
+   */
+  public function testAssert() {
+    $this->assertTrue(TRUE);
+  }
+
+}
