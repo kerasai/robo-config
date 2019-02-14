39e77ce511f134cc5d508b6d2997668aee9223b6
Will Long <will@kerasai.com>
2019-02-14 17:19:40 -0600

    added config helper trait

---
+++ src/ConfigHelperTrait.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kerasai\Robo\Config;
+
+use Robo\Robo;
+
+/**
+ * Trait ConfigHelperTrait.
+ */
+trait ConfigHelperTrait {
+
+  /**
+   * Gets a config value.
+   *
+   * @param string $name
+   *   Config value name.
+   * @param mixed $default
+   *   The default configuration value in the case that no value exists.
+   *
+   * @return mixed
+   *   The configuration value.
+   */
+  public function getConfigVal($name, $default = NULL) {
+    return Robo::Config()->get($name, $default);
+  }
+
+  /**
+   * Require a config value.
+   *
+   * @param string $name
+   *   Config value name.
+   *
+   * @return mixed
+   *   The config value.
+   *
+   * @throws \InvalidArgumentException
+   *   When the configuration does not exist.
+   */
+  public function requireConfigVal($name) {
+    $val = $this->getConfigVal($name);
+    if ($val === NULL) {
+      throw new \InvalidArgumentException(sprintf('Configuration "%s" not found.', $name));
+    }
+    return $val;
+  }
+
+  /**
+   * Gets multiple configuration values.
+   *
+   * @param array $config
+   *   Config to be loaded, keyed by config name, values are defaults.
+   *
+   * @return array
+   *   The config values.
+   */
+  public function getConfigVals(array $config) {
+    return array_map(function ($item, $key) {
+      return $this->getConfigVal($key, $item);
+    }, $config);
+  }
+
+}
